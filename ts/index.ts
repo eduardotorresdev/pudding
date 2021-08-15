2138c654a33d79f39a7aa454cb466c25a42fcd8a
Eduardo Torres <contato@eduardotorres.dev>
2021-08-15 17:47:00 -0300

    feat: slider

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,6 @@
 import '../sass/main.sass';
 import './dropdown';
 import './transformation';
+import './slider';
 
 console.log('Hello World');
---
+++ ts/slider.ts
@@ -0,0 +1,17 @@
+const sliders = document.querySelectorAll('.slider');
+
+sliders.forEach((slider) => {
+    const input: HTMLInputElement | null =
+        slider.querySelector('.slider__input');
+    const progress = slider.querySelector('.slider__progress');
+    const marker = slider.querySelector('.slider__marker');
+
+    input.addEventListener('input', () => {
+        const value = parseInt(input.value);
+        const max = parseInt(input.max);
+        const percentage = (value / max) * 100;
+
+        progress.setAttribute('style', `width: ${percentage}%`);
+        marker.setAttribute('style', `left: ${percentage}%`);
+    });
+});
